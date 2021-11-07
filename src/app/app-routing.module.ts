01cde13b1c89385cf20100b3bd350662b4f2a194
Joel Hernández <lg13hm@gmail.com>
2021-11-07 02:00:34 -0600

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CrudComponent } from './components/crud/crud.component';
+import { DireccionComponent } from './components/crud/direccion/direccion.component';
+import { ProductosComponent } from './components/crud/productos/productos.component';
+import { SucursalesComponent } from './components/crud/sucursales/sucursales.component';
+import { LoginComponent } from './components/login/login.component';
+import { InicioComponent} from './components/menu/inicio/inicio.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { EmpleadosComponent } from './components/crud/empleados/empleados.component';
+import { ProveedoresComponent } from './components/crud/proveedores/proveedores.component';
+import { CartComponent } from './components/cart/cart.component';
+import { UsuarioComponent } from './components/crud/usuario/usuario.component';
+import { ClienteComponent } from './components/crud/cliente/cliente.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent,children:[
+    {path: 'inicio', component: InicioComponent},
+  ]},
+  {path: 'cart', component: CartComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'crud', component: CrudComponent,children:[
+    {path: 'direccion', component: DireccionComponent},
+    {path: 'sucursales', component: SucursalesComponent},
+    {path: 'productos', component: ProductosComponent},
+    {path: 'empleados', component: EmpleadosComponent},
+    {path: 'proveedores', component: ProveedoresComponent},
+    {path: 'usuarios', component: UsuarioComponent},
+    {path: 'clientes', component: ClienteComponent},
+  ]
+  },
+  { path: '**', component: PageNotFoundComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
